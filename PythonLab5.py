e1e51d70837d4468284e35c344d49a72c44abaaa
Cory3875 <ccadle99@gmail.com>
2014-10-14 15:11:34 -0500

    First Commit

---
+++ PythonLab5.py
@@ -0,0 +1,27 @@
+#70 Pt
+
+myList = [1,2,3,4,5,6,7,8,9,10] 
+for item in myList:
+    print item
+
+#80 Pt
+print 
+for item in range(11,21,1):
+    print item
+    
+#90 Pt
+print 
+for item in range(99,-1,-2):
+    print item 
+
+#100 Pt
+
+mySecondList = []
+for item in range(0,5,1):
+    userinput = raw_input()
+    mySecondList.append(userinput)
+print 
+for item in mySecondList:
+    print item
+
+
